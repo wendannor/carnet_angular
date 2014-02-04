5453962f07ef9846c221de94112754516e5a0fc4
jlfy <julienlfy@gmail.com>
2014-02-04 22:26:58 +0100

    Récupération du user et stockage en session
    Json reçu et exploité pour récupérer le user que l'on met en session...

--- authentification/verification.php
+++ authentification/verification.php
@@ -1,71 +1,46 @@
 <?php
-function existeUser($login, $password) {
+function getUser($login, $password) {
     
-	// Connexion � la base de donn�es
-	require "config.inc.php";
-	$db = mysql_connect($db_host, $db_login, $db_password);
-	mysql_select_db($db_base, $db);
-	
-	// Cr�ation de la requ�te SQL
-	$sql_login = mysql_real_escape_string($login); // Eviter les injections SQL
-	$sql_password = mysql_real_escape_string($password); // Eviter les injections SQL
-	$query ="SELECT * FROM user WHERE login='$sql_login' AND password='$sql_password'" ;
-	
-    
-	// Ex�cution de la requ�te SQL
-	$result = mysql_query($query, $db);
-	
-    
-	if (mysql_num_rows($result) > 0) {
-        return true;
-    } else {
-        return false;
+    // Connexion à la base de données
+    require "config.inc.php";
+    $db = mysql_connect($db_host, $db_login, $db_password);
+    mysql_select_db($db_base, $db);
+
+    // Création de la requète SQL
+    $sql_login = mysql_real_escape_string($login); // Eviter les injections SQL
+    $sql_password = mysql_real_escape_string($password); // Eviter les injections SQL
+    $query ="SELECT * FROM user WHERE login='$sql_login' AND password='$sql_password'" ;
+
+    // Exécution de la requète SQL
+    $result = mysql_query($query, $db);
+
+    $user = null;
+    while ($row = mysql_fetch_array($result)) {
+        $user['idUser'] = $row['id_user'];
+        $user['login'] = $row['login'];
+        $user['email'] = $row['email'];
+        $user['nomComplet'] = $row['nom_complet'];
     }
     
-    
-    /*
-    if ($login == "dupont" && $password == "dupont") {
-        return true;
-    }
-    return false;
-    */
+    return $user;
 } 
 
 
 
-session_start() ; // initialisation de la session
+session_start();
 $data = json_decode(file_get_contents('php://input'), true);
-print_r($data);
-$login = null;
-if (isset($_POST['login'])){
-    $login = $_POST['login'];
-}
 
-$password = null;
-if (isset($_POST['password'])){
-    $password = $_POST['password'];
-}
+$login    = $data['login'];
+$password = $data['password'];
 
+$user = getUser($login, $password);
 
-$isAuth = false;
-if ($login && $password) {
-	if (existeUser($login, $password)) {
-        $isAuth = true;
-		// sauvegarde de l'identifiant dans la session
-		$_SESSION['login'] = $login ;
-		$_SESSION['nomComplet'] = "Jean Dupont" ;
-	}
-}
-
-if ($isAuth){
-    // On redirige sur la home
-    $user = array();
-    $user['login'] = "j.dupont";
-    $user['nomComplet'] = "Jean Dupont";
-	exit();
+if ($user != null) {
+    // Mise en session de l'objet
+    foreach ($user as $key => $value){
+        $_SESSION[$key] = $value;
+    }
+    echo 'connecté';
 } else {
-    // On redirige sur la page d'authentification
-    echo 'pas co';
-	exit();
+    echo 'pas connecté';
 }
-
