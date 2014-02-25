4614075dd8fb22aede7ca558c8a984b63ae2bb8f
wendannor <jordan.sarre@gmail.com>
2014-02-25 13:52:15 +0100

    Creation of template url for notebook directive

--- app/scripts/directives/notebook.js
+++ app/scripts/directives/notebook.js
@@ -8,25 +8,7 @@ angular.module('carnetApp')
                 notebook: '=',
                 update: '&'
             },
-            template: '<form class="form-horizontal" role="form">' +
-                '<div class="form-group">' +
-                '<label for="inputTitre" class="col-sm-2 control-label">Titre</label>' +
-                '<div class="col-sm-10">' +
-                '<input type="text" class="form-control" id="inputTitre" placeholder="Title" ng-model="notebook.title">'+
-                '</div>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="inputText" class="col-sm-2 control-label">Texte</label>' +
-                '<div class="col-sm-10">' +
-                '<textarea class="form-control" id="inputText" ng-model="notebook.description"></textarea>' +
-                '</div>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<div class="col-sm-offset-2 col-sm-10">' +
-                '<button type="submit" class="btn btn-primary btn-lg" ng-click="update({notebook:notebook})">Save</button>' +
-                '</div>' +
-                '</div>' +
-                '</form>',
+            templateUrl: 'scripts/directives/notebook-template.html',
             link: function (scope, element, attrs) {
             }
         };
